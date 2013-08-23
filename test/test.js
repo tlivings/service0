555da5e6e0099e42010ce5171aedc3aa3acd646f
Trevor <tlivings@gmail.com>
2013-08-23 10:12:41 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -6,40 +6,50 @@ var assert = require("assert"),
 
 describe("test", function() {
 
-	var TestService;
-
 	before(function(next) {
-
-		TestService = function TestService() {
-			TestService.super_.apply(this);
-		};
-
-		services.extend(TestService, services.Service, {
-
-			doService : function(message) {
-				return "Hello.";
-			}
-
-		});
-
 		next();
 	});
 
-	it("should do something", function(next) {
+	it("should send Hello to client", function(next) {
 
-		var socket = zmq.socket("req");
-
-		var testService = new TestService();
+		var testService = new services.Service();
 
 		testService.bind("inproc://test", function() {
 			var client = new services.Client();
 
-			client.send("inproc://test", "Hello World!", function(error, result) {
-				console.log(error || result);
+			client.send("inproc://test", "Hello World!", function(error, headers, body) {
+				assert(!error);
+                assert(body === "Hello");
 				testService.destroy();
 				next();
 			});
 		});
+
+        testService.on("message", function(headers, body, callback) {
+            callback(null, "Hello");
+        });
+
 	});
+
+    it("should send error to client", function(next) {
+
+        var testService = new services.Service();
+
+        testService.bind("inproc://test", function() {
+            var client = new services.Client();
+
+            client.send("inproc://test", "Hello World!", function(error, headers, body) {
+                assert(error);
+                assert(error.message === "Error!");
+                testService.destroy();
+                next();
+            });
+        });
+
+        testService.on("message", function(headers, body, callback) {
+            callback("Error!");
+        });
+
+    });
 
 });
